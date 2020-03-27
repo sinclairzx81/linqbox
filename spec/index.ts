54e46b13d51e592bcfe7af3737a1194285c7d459
sinclairzx81 <haydn.developer@gmail.com>
2020-03-27 21:48:02 +1300

    publish

--- spec/index.ts
+++ spec/index.ts
@@ -7,33 +7,28 @@ const users = [
   { userid: 3, name: 'roger' },
 ]
 const records = [
-  { recordid: 0, userid: 0, data : 'toaster' },
-  { recordid: 1, userid: 2, data : 'fridge' },
-  { recordid: 2, userid: 1, data : 'television' },
-  { recordid: 3, userid: 4, data : 'toaster' },
-  { recordid: 4, userid: 2, data : 'stove' },
-  { recordid: 5, userid: 0, data : 'couch' },
-  { recordid: 6, userid: 2, data : 'computer' },
-  { recordid: 7, userid: 2, data : 'washing machine' },
-  { recordid: 8, userid: 3, data : 'remote control' },
-  { recordid: 9, userid: 1, data : 'air conditioner' },
+  { recordid: 0, userid: 0, data: 'toaster' },
+  { recordid: 1, userid: 2, data: 'fridge' },
+  { recordid: 2, userid: 1, data: 'television' },
+  { recordid: 3, userid: 4, data: 'toaster' },
+  { recordid: 4, userid: 2, data: 'stove' },
+  { recordid: 5, userid: 0, data: 'couch' },
+  { recordid: 6, userid: 2, data: 'computer' },
+  { recordid: 7, userid: 2, data: 'washing machine' },
+  { recordid: 8, userid: 3, data: 'remote control' },
+  { recordid: 9, userid: 1, data: 'air conditioner' },
 ]
 
-const query = linq `
+const query = linq`
   from user in ${users}
   join record in ${records}
     on user.userid equals record.userid 
       into records
-  orderby user.name
   select {
     user,
-    records: [
-        ...from n in records 
-           orderby n.data 
-           select n
-        ]
+    records
   }`
 
-for(const value of query) {
+for (const value of query) {
   console.log(value)
 }
